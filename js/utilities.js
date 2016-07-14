c96f8ae1698e14a14166dc7a7cffa4beb3b0b5c3
spaz612 <andrewwlong@gmail.com>
2016-07-14 18:49:14 -0500

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -6,7 +6,19 @@ function colorRed(){
   $("#dpad-touch-target").css("background-color", "red");
 }
 
+function loadXML(){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      $("#message").text(xmlhttp.responseText);
+    }  
+  };
+  xmlhttp.open("GET","xml/data.xml",true);
+  xmlhttp.send();
+}
+
 $(document).ready(function(){
   $(document).keydown(colorGreen);
   $(document).keyup(colorRed);
+  loadXML();
 });
