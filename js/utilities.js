279a95c22660516740180cdb472241e1a5829cff
spaz612 <andrewwlong@gmail.com>
2016-07-14 03:15:34 -0500

    Create utilities.js

---
+++ js/utilities.js
@@ -0,0 +1,12 @@
+function colorGreen(){
+  $(div).css("background-color: green");
+}
+
+function colorRed(){
+  $div).css("background-color: red");
+}
+
+$(document).ready(function(){
+  $(document).keydown(colorGreen);
+  $(document).keyup(colorRed);
+});
