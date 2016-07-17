b8959338ee9b705cb42c4fc404100d4bc499c7a9
spaz612 <andrewwlong@gmail.com>
2016-07-16 21:44:54 -0500

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -8,7 +8,7 @@ function colorRed(){
 
 function displayKeypress(code){
   $("#message").text("Display: " + code);
-  var c=$("#monitor");
+  var c=document.getElementById("monitor");
   var ctx=c.getContext("2d");
   ctx.fillRect(20,20,150,100);
 //  var ctx = $("#monitor").getContext("2d");
