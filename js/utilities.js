4e01d10ebb9520fd50372480bd07f5c418506227
spaz612 <andrewwlong@gmail.com>
2016-07-16 21:42:56 -0500

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -12,8 +12,8 @@ function displayKeypress(code){
   var ctx=c.getContext("2d");
   ctx.fillRect(20,20,150,200);
 //  var ctx = $("#monitor").getContext("2d");
-  ctx.font = "30px Arial";
-  ctx.fillText("Code: " + code,80,60);
+//  ctx.font = "30px Arial";
+//  ctx.fillText("Code: " + code,80,60);
 }
 
 function loadXML(){
