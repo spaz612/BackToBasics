190e628a3d134d8d76f16033fd1919e6ba108bb1
spaz612 <andrewwlong@gmail.com>
2016-07-16 21:11:27 -0500

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -8,7 +8,7 @@ function colorRed(){
 
 function displayKeypress(code){
   var ctx = $("#monitor").getContext("2d");
-//  ctx.font = "30px Arial";
+  ctx.font = "30px Arial";
 //  ctx.fillText(code,80,60);
 }
 
