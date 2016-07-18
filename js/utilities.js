79c16a667d396e5fbaa8e03e4363c7fc2024b47e
spaz612 <andrewwlong@gmail.com>
2016-07-18 02:24:39 -0500

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -30,7 +30,7 @@ function paintDriver(speed){
     var sprites = document.getElementById("front-sprite");
     cfx.fillStyle = "#FFFFFF";
     cfx.fillRect(0,0,640,480);
-    this.xVal += run;
+    this.xVal = this.xVal + run;
     alert("x");
     if(this.xVal >= 640){
       run = -run;
