cd23deb04bd1a0685dae0cb7838718bb4e85baa6
spaz612 <andrewwlong@gmail.com>
2016-07-18 02:17:15 -0500

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -81,13 +81,13 @@ function loadXML(){
 }
 
 $(document).ready(function(){
-//  var painter = new paintDriver(5);
-//  var drive = new timeDrive(painter.paintFrame,2);
+  var painter = new paintDriver(5);
+  var drive = new timeDrive(painter.paintFrame,2);
   $(document).keydown(function(event){
     alert("Keydown Event");
     displayKey(event.which);
-//    painter.changeImage(event.which);
+    painter.changeImage(event.which);
   });
   loadXML();
-//  drive.start();
+  drive.start();
 });
